9416a1d33918ad6404628cdbc8e5d350f4c90fd9
Matheus-Ribeir <matheus.ribeiro@sptech.school>
2022-11-07 01:15:47 -0300

    Tela de jogo

---
+++ site/public/js/game.js
@@ -0,0 +1,48 @@
+function startTimer(duration, display){
+    var timer = duration, minutes, seconds;
+
+
+setInterval(function(){
+minutes  = parseInt(timer / 60, 10);
+seconds  = parseInt(timer % 60, 10);
+
+minutes = minutes < 10 ? "0" + minutes : minutes;
+seconds = seconds < 10 ? "0" + seconds : seconds;
+
+
+display.textContent = minutes + ":" + seconds;
+
+if(--timer < 0){
+    timer = duration;
+}
+},1000);
+
+}
+
+
+//codigo game
+var totalMorangos = 0
+var gifMorango = "./assets/img/gif/Strawberry.gif"
+function comecarJogo() {
+      var duration = 60*1; //
+      var display = document.querySelector("#timer")
+      var timerAtivo = document.querySelector("#timer").innerHTML;
+      console.log(timerAtivo)
+    if(timerAtivo == startTimer(duration, display)){
+    
+    }
+        else{
+            var numeroAleatorio = Number(Math.random() * 100).toFixed(0);
+            console.log("Botão clicado" + numeroAleatorio)
+            if (numeroAleatorio != 0) {
+                localMorango = document.getElementById("um");
+            
+            }
+        }
+    
+
+}
+function pegarMorango() {
+    totalMorangos++
+    pontos.innerHTML = totalMorangos
+}
